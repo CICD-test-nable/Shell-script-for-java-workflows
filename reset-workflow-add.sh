723b8d2bbcd589ce7968f640cbac18d12846a380
imeshsen <imeshsenanayake1996@gmail.com>
2023-11-16 17:05:58 +0530

    Modify script

--- reset-workflow-add.sh
+++ reset-workflow-add.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ORG_NAME="CICD-test-nable"
+branch="spims"
 repos=$(gh repo list -l java -q '.[].name' --json name -L 400 $ORG_NAME)
 
 for repo in $repos; do
-	repo_url=https://github.com/CICD-test-nable/$repo.git
+	repo_url=https://github.com/$ORG_NAME/$repo.git
 	test -d $repo || git clone $repo_url
-	git -C $repo checkout master
+	git -C $repo checkout $branch
 	git -C $repo reset --hard origin/no-workflow
 	git -C $repo push -f
 done
