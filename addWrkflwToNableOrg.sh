d1d25f1f0b05a719aec47c545f47b15fa22cd01b
imeshsen <imeshsenanayake1996@gmail.com>
2023-11-16 16:09:40 +0530

    Add new script

---
+++ addWrkflwToNableOrg.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ORG_NAME="CICD-test-nable"
+
+repos=$(gh repo list -q '.[].name' --json name -L 400 $ORG_NAME)
+
+for repo in $repos; do
+
+	repo_url=https://github.com/$ORG_NAME/$repo.git
+
+	test -d $repo || git clone $repo_url
+
+	result=$(./determineprojecttype.sh $repo)
+
+if [[ "$result" == "JAVA" ]]
+
+then
+    cd $repo
+     git checkout -b spims
+     echo "crated spims barnch"
+     mkdir -p .github/workflows
+     cp ../workflow.yml .github/workflows/
+     git add .
+     git commit -m "Add workflow file"
+     git push -u origin spims
+     cd ..
+i
+else
+    echo "This is a non-java repository"
+fi
+
+done
